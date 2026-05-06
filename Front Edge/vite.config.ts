db6381f765e6d5e710fe8d95338f693017df69d2
VINICIUS PRATI MACHADO <viniprati6503@gmail.com>
2026-05-06 20:20:18 -0300

    COnfiguração do vite

--- Front Edge/vite.config.ts
+++ Front Edge/vite.config.ts
@@ -1,3 +1,21 @@
-﻿import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
+import { resolve } from 'node:path'
 
-export default defineConfig({})
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        trilhas: resolve(__dirname, 'trilhas.html'),
+        roadmap: resolve(__dirname, 'roadmap.html'),
+        exercicios: resolve(__dirname, 'exercicios.html'),
+        desafios: resolve(__dirname, 'desafios.html'),
+        projetos: resolve(__dirname, 'projetos.html'),
+        docs: resolve(__dirname, 'docs.html'),
+        'mini-ide': resolve(__dirname, 'mini-ide.html'),
+        modulos: resolve(__dirname, 'modulos.html'),
+        sobre: resolve(__dirname, 'sobre.html')
+      }
+    }
+  }
+})
